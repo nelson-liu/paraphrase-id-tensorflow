aefd972c7fb423396f59da03a1d460cd3559d1e1
Nelson Liu <nelson.liu.2009@gmail.com>
2017-03-18 15:23:03 -0700

    Remove unnecesssary comments of old function signatures

--- duplicate_questions/data/tokenizers/word_tokenizers.py
+++ duplicate_questions/data/tokenizers/word_tokenizers.py
@@ -7,15 +7,12 @@ class SpacyWordTokenizer():
         import spacy
         self.en_nlp = spacy.load('en')
 
-    # def tokenize(self, sentence: str) -> List[str]:
     def tokenize(self, sentence):
         return [str(token.lower_) for token in self.en_nlp.tokenizer(sentence)]
 
-    # def get_words_for_indexer(self, text: str) -> List[str]:
     def get_words_for_indexer(self, text):
         return self.tokenize(text)
 
-    # def index_text(self, text: str, data_indexer: DataIndexer) -> List:
     def index_text(self, text, data_indexer):
         return [data_indexer.get_word_index(word) for word in
                 self.get_words_for_indexer(text)]
