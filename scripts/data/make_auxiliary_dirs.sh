ec3dd62d8bbd61c5d1398895209db1e0fab18236
Nelson Liu <nelson-liu@users.noreply.github.com>
2017-04-28 00:34:25 -0700

    Remove data/logs/models directories, add script to make them (#27)
    * Remove empty dirs for data, logs, models
    
    * Edit gitignore to ignore root data, models, logs
    
    * Add script to make auxiliary directories

---
+++ scripts/data/make_auxiliary_dirs.sh
@@ -0,0 +1,8 @@
+echo "Creating auxiliary directories"
+mkdir -p ../../data/external
+mkdir -p ../../data/interim
+mkdir -p ../../data/processed
+mkdir -p ../../data/raw
+mkdir -p ../../logs/
+mkdir -p ../../models/
+echo "Done creating auxiliary directories"
