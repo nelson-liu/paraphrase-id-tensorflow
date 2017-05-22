ad68112ad64ee83644bb25d08ae8917f6a91afc7
Gareth Dwyer <sixhobbits@users.noreply.github.com>
2017-05-22 02:13:19 +0200

    Aux directories get created in the wrong place (#58)
    * Aux directories get created in the wrong place
    
    With the current instructions in the README, you are told to call make aux_dirs, which calls this script. The directories are all then created two levels too high in the tree, as they are created relative to the main directory.
    
    * Add warning about using script only by Makefile

--- scripts/data/make_auxiliary_dirs.sh
+++ scripts/data/make_auxiliary_dirs.sh
@@ -1,13 +1,12 @@
-# cd to the path of this bash file
-parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
-
-cd "$parent_path"
+# Note that this script should NOT be run directly 
+# (e.g. with `./make_auxiliary_dirs.sh`),
+# but rather run through the Makefile (with `make aux_dirs`)
 
 echo "Creating auxiliary directories"
-mkdir -p ../../data/external
-mkdir -p ../../data/interim
-mkdir -p ../../data/processed
-mkdir -p ../../data/raw
-mkdir -p ../../logs/
-mkdir -p ../../models/
+mkdir -p ./data/external
+mkdir -p ./data/interim
+mkdir -p ./data/processed
+mkdir -p ./data/raw
+mkdir -p ./logs/
+mkdir -p ./models/
 echo "Done creating auxiliary directories"
